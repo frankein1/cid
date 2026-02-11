d21a2bcec1ab27870834b03db911f315dffb7983
frankein1 <frankein1313@gmail.com>
2026-02-11 15:45:18 +0100

    Create vins.py

---
+++ core/views/vins.py
@@ -0,0 +1,12 @@
+
+from django.http import HttpResponse
+from django.contrib.admin.views.decorators import staff_member_required
+from django.core.management import call_command
+
+@staff_member_required
+def run_install(request):
+    try:
+        call_command("install")
+        return HttpResponse("Installation terminée !")
+    except Exception as e:
+        return HttpResponse(f"Erreur lors de l'installation : {e}", status=500)
