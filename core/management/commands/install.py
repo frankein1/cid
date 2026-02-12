994b73ccc81dd15171c2a81a9e736d69e406c497
frankein1 <frankein1313@gmail.com>
2026-02-12 14:43:52 +0100

    Update install.py

--- core/management/commands/install.py
+++ core/management/commands/install.py
@@ -84,16 +84,15 @@ class Command(BaseCommand):
         # ETAPE C : MDS
         # ============================================================
         if etape in ("", "mds"):
-    self.stdout.write("\n📌 Étape : INITIALISATION MDS")
-
-    try:
-        from mds.init_mds import init_mds
-        log = init_mds()
-        self.stdout.write(log)
-        self.stdout.write(self.style.SUCCESS("   ✅ MDS OK"))
-    except Exception as e:
-        self.stdout.write(self.style.ERROR(f"   ❌ Erreur MDS: {e}"))
-        return
+            self.stdout.write("\n📌 Étape : INITIALISATION MDS")
+            try:
+                from mds.init_mds import init_mds
+                log = init_mds()
+                self.stdout.write(log)
+                self.stdout.write(self.style.SUCCESS("   ✅ MDS OK"))
+            except Exception as e:
+                self.stdout.write(self.style.ERROR(f"   ❌ Erreur MDS: {e}"))
+                return
 
         # ============================================================
         # ETAPE D : PLANNING
