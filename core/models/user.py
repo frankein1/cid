93887b49a3983e24ae72bd93319255d90d79fe91
frankein1 <frankein1313@gmail.com>
2026-05-04 01:54:44 +0200

    Reduce max_length of matricule field to 15

--- core/models/user.py
+++ core/models/user.py
@@ -58,7 +58,7 @@ class User(TimestampedMixin, AbstractUser):
     objects = CustomUserManager()
     
     # Identifiants
-    matricule = models.CharField(max_length=20, unique=True, null=False, blank=False)
+    matricule = models.CharField(max_length=15, unique=True, null=False, blank=False)
     
     # Coordonnées
     telephone_professionnel = models.CharField(max_length=20, blank=True, default='')
