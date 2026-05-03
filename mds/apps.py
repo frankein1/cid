3c1d9f1fcc8c6f67cfe526e127aa0971f276e1bf
frankein1 <frankein1313@gmail.com>
2026-05-03 22:54:15 +0200

    Refactor permissions import and connect post_migrate signal
    Replaced direct import of permissions with a deferred import to avoid circular dependencies and connected the post_migrate signal explicitly.

--- mds/apps.py
+++ mds/apps.py
@@ -12,7 +12,13 @@ class MaisonDptaleSolidariteConfig(AppConfig):
         Méthode exécutée au démarrage de Django.
         On y importe les permissions pour connecter le signal post_migrate.
         """
-        import mds.permissions
+        # Import différé pour éviter les cycles
+        from mds.permissions import create_mds_permissions
+        from django.db.models.signals import post_migrate
+        
+        # Connexion explicite du signal
+        post_migrate.connect(create_mds_permissions, sender=self)
+
 
 class PlanningConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
