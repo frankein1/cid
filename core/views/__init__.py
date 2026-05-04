a5cb5aaaf8aab11f9a15dedf926b3e438a38f3bd
frankein1 <frankein1313@gmail.com>
2026-05-04 20:30:53 +0200

    Remove unused import from __init__.py
    Remove trigger_test_user import to avoid confusion.

--- core/views/__init__.py
+++ core/views/__init__.py
@@ -7,6 +7,5 @@ Permet d'accéder aux vues via core.views.nom_vue
 
 from .main import dashboard, parametres
 from .profile import mon_profil
-from .tests import trigger_test_user
 # Ne pas exporter ajax_ville_par_cp ici pour éviter la confusion
 # Il est importé directement depuis core.views.ajax dans urls.py
