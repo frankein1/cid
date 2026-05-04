33be40d3cbc767f30f7979281972f6eb6429a102
frankein1 <frankein1313@gmail.com>
2026-05-04 02:13:19 +0200

    Add model imports and __all__ definition
    Import user, organisation, profil, and capacite models for core application.

--- core/__init__.py
+++ core/__init__.py
@@ -2,4 +2,9 @@
 """
 Application CORE - Fondation du SI DITAS
 """
-default_app_config = 'core.apps.CoreConfig'
+from .models.user import User
+from .models.organisation import Service
+from .models.profil import Profil
+from .models.capacite import Capacite
+
+__all__ = ['User', 'Service', 'Profil', 'Capacite']
