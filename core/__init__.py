b0bf8703206c84d9b7bc1f2e2084141a1a5ebadd
frankein1 <frankein1313@gmail.com>
2026-05-04 02:45:47 +0200

    Add default app configuration for core module

--- core/__init__.py
+++ core/__init__.py
@@ -2,6 +2,8 @@
 """
 Application CORE - Fondation du SI DITAS
 """
+default_app_config = 'core.apps.CoreConfig'
+
 from .models.user import User
 from .models.organisation import Service
 from .models.profil import Profil
