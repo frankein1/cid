fca59ca139df83d8a9fdf1c83d514b2e4e9caa85
frankein1 <frankein1313@gmail.com>
2026-05-05 00:58:50 +0200

    Add licensing information to signals.py
    Added licensing comments to the top of signals.py.

--- AidFi/signals.py
+++ AidFi/signals.py
@@ -1,3 +1,8 @@
+# =============================================================================
+# © AGPL3 - CID - Developpeur : Frederic COTTA
+# Assistance technique: les IA et particulièrement DeepSeek 
+# Interdiction de réutilisation commerciale
+# =============================================================================
 # AidFi/signals.py
 
 from django.db.models.signals import post_save
