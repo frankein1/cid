47da56366e6cfc0d3abdf063ef99441439f3cb01
frankein1 <frankein1313@gmail.com>
2026-05-04 04:10:38 +0200

    Update exports in core.views to clarify usage
    Removed ajax_ville_par_cp from exports to avoid confusion.

--- core/views/__init__.py
+++ core/views/__init__.py
@@ -1,5 +1,11 @@
 # core/views/__init__.py
 
+"""
+Exports des vues pour l'application core
+Permet d'accéder aux vues via core.views.nom_vue
+"""
+
 from .main import dashboard, parametres
 from .profile import mon_profil
-from .ajax import ajax_ville_par_cp
+# Ne pas exporter ajax_ville_par_cp ici pour éviter la confusion
+# Il est importé directement depuis core.views.ajax dans urls.py
