8c34a0080aa0ca5c97a209f2674ad2d4b94e32e1
frankein1 <frankein1313@gmail.com>
2026-05-04 23:39:53 +0200

    Remove conditional queryset assignment for documents_ged

--- AidFi/forms/__init__.py
+++ AidFi/forms/__init__.py
@@ -1,2 +1,2 @@
 # AidFi/forms/__init__.py
-if beneficiaire: self.fields['documents_ged'].queryset = DocumentGED.objects.filter(content_type=ContentType.objects.get_for_model(beneficiaire), object_id=beneficiaire.id)
+
